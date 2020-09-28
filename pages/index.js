6d9d9c1ea944dbebf843ad9ee93d2e1354436626
rakumairu <demaspira@outlook.com>
2020-09-28 19:17:24 +0700

    initial commit

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,89 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import { useState } from 'react'
 
 export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    const [videos, setVideos] = useState(['Haha', 'Haha', 'Haha', 'Haha'])
+    const [isPresenting, setIsPresenting] = useState(false)
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+    const add = () => {
+        setVideos(prev => prev.concat(['Haha']))
+    }
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+    const remove = () => {
+        if (videos.length > 1) setVideos(prev => prev.slice(0, prev.length - 1))
+    }
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
-  )
+    return (
+        <>
+            <Head>
+                <style>
+                    {`
+                        #__next { height: 100% }
+                    `}
+                </style>
+            </Head>
+            <div
+                style={{
+                    position: "absolute",
+                    right: 0,
+                    top: '50%',
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    marginRight: 8,
+                    fontSize: 24,
+                    transform: 'translateY(-50%)'
+                }}
+            >
+                <span onClick={()=> add()}>+</span>
+                <span>{ videos.length }</span>
+                <span onClick={()=> remove()}>-</span>
+            </div>
+            <span
+                style={{
+                    position: 'absolute',
+                    right: 0,
+                    bottom: 0,
+                    fontSize: 32,
+                    fontWeight: 700,
+                    color: isPresenting ? 'green' : 'red'
+                }}
+                onClick={() => setIsPresenting(prev => !prev)}
+            >
+                presenting
+            </span>
+            <div
+                style={{
+                    minHeight: '100%',
+                    minWidth: '100%',
+                    height: '100%',
+                    width: '100%',
+                    padding: '1rem',
+                    display: "grid",
+                    alignContent: 'center',
+                    gridAutoRows: '33%',
+                    gridTemplateColumns: videos.length > 3 || isPresenting ? '1fr 1fr 1fr' : '1fr '.repeat(videos.length),
+                }}
+            >
+                {
+                    videos.slice(0,isPresenting ? 4 : 9).map((video, index) => (
+                        <div
+                            key={index}
+                            style={{
+                                alignItems: 'center',
+                                justifyContent: 'center',
+                                display: 'flex',
+                                border: '1px solid #d7d7d7',
+                                gridColumn: isPresenting && index === 0 ? videos.length === 1 ? '1/4' : '1/3' : '',
+                                gridRow: isPresenting ? index === 0 ? '1/4' : '' : videos.length < 3 ? '1/4' : ''
+                            }}
+                        >
+                            { video }{ index }
+                        </div>
+                    ))
+                }
+            </div>
+        </>
+    )
 }
